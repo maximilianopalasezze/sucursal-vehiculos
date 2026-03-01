7a93f317fc79076af8982fb2461fdc942c458f9d
maximilianopalasezze <palasezzemaxi@gmail.com>
2026-03-01 18:30:53 -0300

    Primer commit - VehiculosRest

---
+++ src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
@@ -0,0 +1,13 @@
+package com.vehiculos.VehiculosRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class VehiculosRestApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiculosRestApplication.class, args);
+    }
+}
